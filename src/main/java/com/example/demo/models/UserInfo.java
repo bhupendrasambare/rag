2057e5f352389ecbc909ddc47a3a25fb03713452
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-07-12 22:47:01 +0530

    users entity updated

--- src/main/java/com/example/demo/models/UserInfo.java
+++ src/main/java/com/example/demo/models/UserInfo.java
@@ -6,8 +6,41 @@
  **/
 package com.example.demo.models;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Table(name = "user_info")
 public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password_hash")
+    private String passwordHash;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
