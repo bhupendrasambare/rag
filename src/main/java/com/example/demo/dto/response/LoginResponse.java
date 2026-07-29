02ebc21692a02a2d3b86c95f14ab2c772b0fca10
bhupendrasambare <bhupendrasam1404@gmail.com>
2026-07-29 07:57:08 +0530

    checkstyle and file headers added

--- src/main/java/com/example/demo/dto/response/LoginResponse.java
+++ src/main/java/com/example/demo/dto/response/LoginResponse.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) ${YEAR} Bhupendra Sambare
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package com.example.demo.dto.response;
 
 import lombok.AllArgsConstructor;
@@ -11,14 +29,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginResponse {
 
-    private String accessToken;
+  private String accessToken;
 
-    private String refreshToken;
+  private String refreshToken;
 
-    private Long expiresIn;
+  private Long expiresIn;
 
-    @Builder.Default
-    private String tokenType = "Bearer";
+  @Builder.Default private String tokenType = "Bearer";
 
-    private UserResponse user;
+  private UserResponse user;
 }
--- src/main/java/com/example/demo/dto/response/UserProfileResponse.java
+++ src/main/java/com/example/demo/dto/response/UserProfileResponse.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) ${YEAR} Bhupendra Sambare
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
 package com.example.demo.dto.response;
 
 import com.example.demo.constants.UserRole;
@@ -14,19 +32,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserProfileResponse {
 
-    private UUID id;
+  private UUID id;
 
-    private String firstName;
+  private String firstName;
 
-    private String lastName;
+  private String lastName;
 
-    private String email;
+  private String email;
 
-    private String profileImage;
+  private String profileImage;
 
-    private UserRole role;
+  private UserRole role;
 
-    private Boolean active;
+  private Boolean active;
 
-    private LocalDateTime createdAt;
+  private LocalDateTime createdAt;
 }
